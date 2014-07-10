a93af403b3b28095f026c6b4b5eaf6e086e3abeb
Tim Kendrick <timkendrick@gmail.com>
2014-07-10 04:19:33 +0100

    Remove unnecessary client-side code

--- client/src/app.js
+++ client/src/app.js
@@ -3,53 +3,16 @@
 var app = angular.module('test-app', ['services']);
 
 
-app.controller('TestCtrl', function TestCtrl($scope, DocketService, QuoteService, numQuotes) {
+app.controller('TestCtrl', function TestCtrl($scope, QuoteService, quoteIds) {
+		$scope.featuredQuote = QuoteService.retrieve(1).subscribe(['value1', 'value2', 'value3']);
+		$scope.quotes = quoteIds.map(function(quoteId) {
+			return QuoteService.retrieve(quoteId).subscribe();
+		});
 
-		_initFeaturedQuote();
-		_initAllQuotes();
-
-
-		function _initFeaturedQuote() {
-			$scope.featuredQuote = null;
-
-			var featuredQuoteSubscription = QuoteService.retrieve(1)
-				.done(function(model) { $scope.featuredQuote = model; })
-				.subscribe(['value1', 'value2', 'value3']);
-
-
-			$scope.$on('destroy', function() {
-				featuredQuoteSubscription.cancel();
-			});
-		}
-
-
-		function _initAllQuotes() {
-			var quoteSubscriptions = _loadQuotes(numQuotes);
-
-			var quoteValues = quoteSubscriptions.map(function(quoteSubscription, index) {
-				quoteSubscription.ready(function(model) { quoteValues[index] = model; });
-				return { };
-			});
-
-			$scope.$on('$destroy', function() {
-				quoteSubscriptions.forEach(function(quoteSubscription) {
-					quoteSubscription.cancel();
-				});
-			});
-
-			$scope.allQuotes = quoteValues;
-
-
-			function _loadQuotes(numQuotes) {
-				var quoteSubscriptions = [];
-				for (var i = 0; i < numQuotes; i++) {
-					var quoteId = i + 1;
-					var quoteSubscription = QuoteService.retrieve(quoteId).subscribe();
-					quoteSubscriptions.push(quoteSubscription);
-				}
-				return quoteSubscriptions;
-			}	
-		}
+		$scope.$on('$destroy', function() {
+			$scope.featuredQuote.cancel();
+			$scope.quotes.forEach(function(quote) { quote.cancel(); });
+		});
 	}
 );
 
@@ -77,30 +40,4 @@ app.service('QuoteService', function(ModelService) {
 	}
 
 	return new QuoteService();
-});
-
-
-app.service('DocketService', function(ModelService) {
-
-	function DocketService() {
-		var MODEL_TYPE = 'docket';
-
-		this.create = function(model) {
-			return ModelService.create(MODEL_TYPE, model);
-		};
-
-		this.retrieve = function(id) {
-			return ModelService.retrieve(MODEL_TYPE, id);
-		};
-
-		this.update = function(id, model) {
-			return ModelService.update(MODEL_TYPE, id, model);
-		};
-
-		this.delete = function(id, subscribe) {
-			return ModelService.delete(MODEL_TYPE, id);
-		};
-	}
-
-	return new DocketService();
 });
